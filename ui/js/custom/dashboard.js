c1cb627f19d7f394c01a47b6c9f3ec75d55a2b3b
Sravan Kumar <sravan.kesa2002@gmail.com>
2026-09-20 19:00:58 +0530

    Fix broken Print button markup and misplaced click handler

--- ui/js/custom/dashboard.js
+++ ui/js/custom/dashboard.js
@@ -5,17 +5,19 @@ $(function () {
             var table = '';
             var totalCost = 0;
             $.each(response, function(index, order) {
-            var orderTotal = parseFloat(order.total);
-            totalCost += orderTotal;
-            table += '<tr>' +
-                '<td>'+ order.datetime +'</td>'+
-                '<td>'+ order.order_id +'</td>'+
-                '<td>'+ order.customer_name +'</td>'+
-                '<td>'+ orderTotal.toFixed(2) +' Rs</td>'+
-                '<td><span class="btn btn-xs btn-primary view-order" data-id="'+ order.order_id +'">View</span></td>'+
-                '<span class="btn btn-xs btn-secondary print-order" data-id="'+ order.order_id +'">Print</span>'+
-                '</tr>';
-        });
+                var orderTotal = parseFloat(order.total);
+                totalCost += orderTotal;
+                table += '<tr>' +
+                    '<td>'+ order.datetime +'</td>'+
+                    '<td>'+ order.order_id +'</td>'+
+                    '<td>'+ order.customer_name +'</td>'+
+                    '<td>'+ orderTotal.toFixed(2) +' Rs</td>'+
+                    '<td>'+
+                    '<span class="btn btn-xs btn-primary view-order" data-id="'+ order.order_id +'">View</span> '+
+                    '<span class="btn btn-xs btn-secondary print-order" data-id="'+ order.order_id +'">Print</span>'+
+                    '</td>'+
+                    '</tr>';
+            });
             table += '<tr><td colspan="3" style="text-align: end"><b>Total</b></td><td><b>'+ totalCost.toFixed(2) +' Rs</b></td><td></td></tr>';
             $("table").find('tbody').empty().html(table);
         }
@@ -30,11 +32,6 @@ $(document).on("click", ".view-order", function () {
     modalBody.html('<img src="https://demo.test.cloint.com/assets/images/spinner.gif" width="40" style="margin: 60px auto;" alt="">');
     $("#myModal").modal('show');
 
-    $(document).on("click", ".print-order", function () {
-        var orderId = $(this).data('id');
-        window.open('receipt.html?order_id=' + orderId, '_blank');
-    });
-    
     $.get(orderDetailsApiUrl, { order_id: orderId }, function (response) {
         var table = '<table class="table table-bordered text-left">' +
             '<thead><th>Product</th><th>Quantity</th><th>Total Price</th></thead><tbody>';
@@ -51,4 +48,10 @@ $(document).on("click", ".view-order", function () {
         table += '</tbody></table>';
         modalBody.html(table);
     });
+});
+
+// Print/PDF receipt: opens a standalone receipt page in a new tab for this order.
+$(document).on("click", ".print-order", function () {
+    var orderId = $(this).data('id');
+    window.open('receipt.html?order_id=' + orderId, '_blank');
 });
